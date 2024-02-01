6346cd87d4b17c9cb6d5c8c93e6004a554686981
Horaira <horaira.khan@gmail.com>
2024-02-01 16:51:01 +0530

    selected right branch

--- prepare-and-run-bahmni.sh
+++ prepare-and-run-bahmni.sh
@@ -3,7 +3,7 @@
 echo "this would be triggered through systemd"
 who >> log.txt
 date >>  log.txt
-git checkout experiment/custom-configuration-on-tag-1.0.0-lite
+git checkout experiment/custom-configuration-basedon-master
 git pull origin
 cd ./bahmni-lite
 pwd >> log.txt
